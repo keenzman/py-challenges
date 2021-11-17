75e916571bc3128d8a717c0956400d011a411571
keenzman <szaman_03@hotmail.co.uk>
2021-11-17 20:09:25 +0000

    complete py5 Foundation challenges

--- py-challenges/py5-list-comprehension-lambdas-part-2/challenge.py
+++ py-challenges/py5-list-comprehension-lambdas-part-2/challenge.py
@@ -9,3 +9,41 @@
   and may require you to google things in order to solve them. If you find an answer online somewhere, be kind and
   share it with the group!
 '''
+
+''' Foundation Challenges '''
+
+'''
+ # A function that uses the REDUCE array iterator to total an array of scores.
+ # Will need to import reduce from functools
+ # The scores will be numbers.
+ #
+ # @param {number[]} numberArr [7, 7, 6, 2, 3, 2, 3]
+ # @return {number} 30
+'''
+from functools import reduce
+
+def totalScoresArr(numberArr):
+  total = reduce(lambda a, b: a+b, numberArr)
+  return total
+
+'''
+ # A function that reverses a string.
+ # It will need to keep spaces between words.
+ #
+ # @param {string} toReverse "reverse this"
+ # @return {string} "siht esrever"
+'''
+def reverseString(toReverse):
+    return toReverse[::-1]
+
+'''
+ # A function that arranges an array of characters alphabetically.
+ # Each character will need to be lowercase.
+ # A to Z case insensitive.
+ #
+ # @param {string[]} charcterArr ["X", "B", "B", "b", "g", "l", "n", "x"]
+ # @return {string[]} ["b", "b", "b", "g", "l", "n", "x", "x"]
+'''
+def sortCharactersAlphabetically(charcterArr):
+  lowerCaseList = map(lambda alphabet: alphabet.lower(), charcterArr)
+  return sorted(lowerCaseList)
--- py-challenges/py5-list-comprehension-lambdas-part-2/challenge_test.py
+++ py-challenges/py5-list-comprehension-lambdas-part-2/challenge_test.py
@@ -3,10 +3,33 @@ import challenge
 
 class ControlFlowTest(unittest.TestCase):
 
-  def test_removeFalseValues(self):
-    booleanList = [True, True, False, False, True]
-    result = challenge.removeFalseValues(booleanList)
-    self.assertEqual(result, [True, True, True])
+  def test_totalScoresArr(self):
+    numList = [7, 7, 6, 2, 3, 2, 3]
+    result = challenge.totalScoresArr(numList)
+    self.assertEqual(result, 30)
+
+    numList2 = [3]
+    result = challenge.totalScoresArr(numList2)
+    self.assertEqual(result, 3)
+
+  def test_reverseString(self):
+    stringToReverse = "reverse this"
+    result = challenge.reverseString(stringToReverse)
+    self.assertEqual(result, "siht esrever")
+
+    stringToReverse2 = "r"
+    result = challenge.reverseString(stringToReverse2)
+    self.assertEqual(result, "r")
+
+  def test_sortCharactersAlphabetically(self):
+    charsArr = ["X", "B", "B", "b", "g", "l", "n", "x"]
+    result = challenge.sortCharactersAlphabetically(charsArr)
+    self.assertEqual(result, ["b", "b", "b", "g", "l", "n", "x", "x"])
+
+    charsArr = ["x"]
+    result = challenge.sortCharactersAlphabetically(charsArr)
+    self.assertEqual(result, ["x"])
+
 
 if __name__ == '__main__':
   unittest.main()
